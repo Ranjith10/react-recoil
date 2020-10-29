79f2e7179366f47a9f8e64eaaf770cd43293aea1
Ranjith kumar <ranjit4072@gmail.com>
2020-10-29 16:54:10 +0530

    Getting started with atom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { atom, useRecoilState} from 'recoil'
+
 import './App.css'
 
-function App() {
-  return (
-    <div className = 'App' >
-        sample
-    </div>
-  )
+const filmsAtom = atom({
+    key: 'films',
+    default: []
+})
+
+const App = () => {
+
+    const [films, setFilms] = useRecoilState(filmsAtom)
+
+    useEffect(() => {
+        const getFilms = async () => {
+            const url = `https://swapi.dev/api/films/`;
+            // eslint-disable-next-line no-undef
+            const resp = await fetch(url);
+            const body = await resp.json();
+            setFilms(body.results)
+        }
+        getFilms()
+    }, [setFilms])
+
+    return (
+        <div className = 'App' >
+            {films.map((film) => {
+                return (
+                    <>
+                        <div>
+                            Title: {film.title}
+                        </div>
+                        <div>
+                            Director: {film.director}
+                        </div>
+                        <div>
+                            Producer: {film.producer}
+                        </div>
+                    </>
+            )})}   
+        </div>
+    )
 }
 
 export default App
